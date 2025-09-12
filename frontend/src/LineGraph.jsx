620ff8a737044bd7f3bfa8d825108dfcbbd04c60
manprajapatie <manprajapatie@gmail.com>
2025-09-12 21:32:57 +0530

    got some issue in LineGraph

---
+++ frontend/src/LineGraph.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+import { Line } from "react-chartjs-2"
+
+const LineGraph = () => {
+  const [data, setData] = useState({})
+
+
+  const buildChartData = (data, casesType = 'cases') => {
+    const chartData = [];
+    let lastDataPoint;
+
+    data[casesType].forEach(date => {
+      if (lastDataPoint) {
+        const newDataPoint = {
+          x: date,
+          y: data[casesType][date] - lastDataPoint
+        }
+        chartData.push(newDataPoint)
+      }
+      lastDataPoint = data[casesType][date]
+    })
+    return chartData
+  }
+
+  useEffect(() => {
+    fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+      .then((response) => response.json())
+      .then((data) => {
+        const chartData = buildChartData(data)
+        setData(chartData)
+      }
+      )
+  }, [])
+
+
+  return (
+    <div>
+      <Line
+        //data takes a object 
+        // data={{}}
+        options />
+    </div>
+  )
+}
+
+export default LineGraph

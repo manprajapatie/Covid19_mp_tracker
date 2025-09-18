e57678998b406cf60501d0cf64a750ef40429d8b
manprajapatie <manprajapatie@gmail.com>
2025-09-18 08:41:11 +0530

    create util file and create functionality in it

---
+++ frontend/src/Util.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import numeral from "numeral";
+import { Circle, Popup } from "react-leaflet";
+
+const casesTypeColors = {
+    cases: {
+        hex: "#CC1034",
+        multiplier: 100,
+    },
+    recovered: {
+        hex: "#7dd71d",
+        multiplier: 150,
+    },
+    deaths: {
+        hex: "#fb4443",
+        multiplier: 300,
+    },
+};
+
+export const sortData = (data) => {
+    const sortedData = [...data]
+    return sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1))
+}
+
+//draw circle on the map with country info with covid cases
+export const showDataOnMap = (data, casesType = "cases") => (
+    data.map(country => (
+        <Circle
+            center={[country.countryInfo.lat, country.countryInfo.long]}
+            fillOpacity={0.4}
+            color={casesTypeColors[casesType].hex}
+            fillColor={casesTypeColors[casesType].hex}
+            radius={Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier}
+        >
+            <Popup>
+                <div className="info-container">
+                    <div className="info-flag" style={{ backgroundImage: `url(${country.countryInfo.flag})` }}/>
+                    <div className="info-name">{country.country}</div>
+                    <div className="info-confirmed">Cases: {numeral(country.cases).format("0, 0")}</div>
+                    <div className="info-recovered">Cases: {numeral(country.recovered).format("0, 0")}</div>
+                    <div className="info-deaths">Cases: {numeral(country.deaths).format("0, 0")}</div>
+                    
+                </div>
+            </Popup>
+        </Circle>
+    )
+    ))
